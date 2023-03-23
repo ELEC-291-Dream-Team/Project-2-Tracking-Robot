63095b5f04a7b4676e7d7706a22e3ba0c8297c69
Peter Woolsey <woolsey@student.ubc.ca>
2023-03-23 13:55:50 -0700

    code now works with hardcoded duty cycle (no inputs)

--- doc/remote.c
+++ doc/remote.c
@@ -1,20 +1,29 @@
+
+/*
+Timer 0: 5kHz ouput
+Timer 2: 1kHz output -> going to be used for the PWM
+
+*/
+
 #include <EFM8LB1.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SYSCLK      72000000L  // SYSCLK frequency in Hz
-#define BAUDRATE      115200L  // Baud rate of UART in bps
+#define SYSCLK 72000000L // SYSCLK frequency in Hz
+#define BAUDRATE 115200L // Baud rate of UART in bps
 
-#define RELOAD_TIMER0 	106
+// #define RELOAD_TIMER0 106
+#define RELOAD_TIMER0 210 //16kHz
 
-#define TIMER_OUT_0	P2_0
+#define TIMER_OUT_0 P2_0
+#define TIMER_OUT_0_INVERTED P2_1
 #define TIMER_OUT_2 P1_6
 
 #define TIMER_2_FREQ = 1000L
 
 #define LCD_RS P2_6
 // #define LCD_RW Px_x // Not used in this code.  Connect to GND
-#define LCD_E  P2_5
+#define LCD_E P2_5
 #define LCD_D4 P2_4
 #define LCD_D5 P2_3
 #define LCD_D6 P2_2
@@ -22,100 +31,109 @@
 #define CHARS_PER_LINE 16
 
 unsigned char overflow_count;
+int pulse_width;
+int pwm_count = 0;
 
-char _c51_external_startup (void)
+char _c51_external_startup(void)
 {
 	// Disable Watchdog with key sequence
 	SFRPAGE = 0x00;
-	WDTCN = 0xDE; //First key
-	WDTCN = 0xAD; //Second key
-  
+	WDTCN = 0xDE; // First key
+	WDTCN = 0xAD; // Second key
+
 	VDM0CN |= 0x80;
 	RSTSRC = 0x02;
 
-	#if (SYSCLK == 48000000L)	
-		SFRPAGE = 0x10;
-		PFE0CN  = 0x10; // SYSCLK < 50 MHz.
-		SFRPAGE = 0x00;
-	#elif (SYSCLK == 72000000L)
-		SFRPAGE = 0x10;
-		PFE0CN  = 0x20; // SYSCLK < 75 MHz.
-		SFRPAGE = 0x00;
-	#endif
-	
-	#if (SYSCLK == 12250000L)
-		CLKSEL = 0x10;
-		CLKSEL = 0x10;
-		while ((CLKSEL & 0x80) == 0);
-	#elif (SYSCLK == 24500000L)
-		CLKSEL = 0x00;
-		CLKSEL = 0x00;
-		while ((CLKSEL & 0x80) == 0);
-	#elif (SYSCLK == 48000000L)	
-		// Before setting clock to 48 MHz, must transition to 24.5 MHz first
-		CLKSEL = 0x00;
-		CLKSEL = 0x00;
-		while ((CLKSEL & 0x80) == 0);
-		CLKSEL = 0x07;
-		CLKSEL = 0x07;
-		while ((CLKSEL & 0x80) == 0);
-	#elif (SYSCLK == 72000000L)
-		// Before setting clock to 72 MHz, must transition to 24.5 MHz first
-		CLKSEL = 0x00;
-		CLKSEL = 0x00;
-		while ((CLKSEL & 0x80) == 0);
-		CLKSEL = 0x03;
-		CLKSEL = 0x03;
-		while ((CLKSEL & 0x80) == 0);
-	#else
-		#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
-	#endif
-	
-	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
-	XBR0     = 0x01; // Enable UART0 on P0.4(TX) and P0.5(RX)                     
-	XBR1     = 0X00;
-	XBR2     = 0x40; // Enable crossbar and weak pull-ups
+#if (SYSCLK == 48000000L)
+	SFRPAGE = 0x10;
+	PFE0CN = 0x10; // SYSCLK < 50 MHz.
+	SFRPAGE = 0x00;
+#elif (SYSCLK == 72000000L)
+	SFRPAGE = 0x10;
+	PFE0CN = 0x20; // SYSCLK < 75 MHz.
+	SFRPAGE = 0x00;
+#endif
 
-	#if (((SYSCLK/BAUDRATE)/(2L*12L))>0xFFL)
-		#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
-	#endif
+#if (SYSCLK == 12250000L)
+	CLKSEL = 0x10;
+	CLKSEL = 0x10;
+	while ((CLKSEL & 0x80) == 0)
+		;
+#elif (SYSCLK == 24500000L)
+	CLKSEL = 0x00;
+	CLKSEL = 0x00;
+	while ((CLKSEL & 0x80) == 0)
+		;
+#elif (SYSCLK == 48000000L)
+	// Before setting clock to 48 MHz, must transition to 24.5 MHz first
+	CLKSEL = 0x00;
+	CLKSEL = 0x00;
+	while ((CLKSEL & 0x80) == 0)
+		;
+	CLKSEL = 0x07;
+	CLKSEL = 0x07;
+	while ((CLKSEL & 0x80) == 0)
+		;
+#elif (SYSCLK == 72000000L)
+	// Before setting clock to 72 MHz, must transition to 24.5 MHz first
+	CLKSEL = 0x00;
+	CLKSEL = 0x00;
+	while ((CLKSEL & 0x80) == 0)
+		;
+	CLKSEL = 0x03;
+	CLKSEL = 0x03;
+	while ((CLKSEL & 0x80) == 0)
+		;
+#else
+#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
+#endif
+
+	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
+	XBR0 = 0x01;	 // Enable UART0 on P0.4(TX) and P0.5(RX)
+	XBR1 = 0X00;
+	XBR2 = 0x40; // Enable crossbar and weak pull-ups
+
+#if (((SYSCLK / BAUDRATE) / (2L * 12L)) > 0xFFL)
+#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
+#endif
 	// Configure Uart 0
 	SCON0 = 0x10;
-	CKCON0 |= 0b_0000_0000 ; // Timer 1 uses the system clock divided by 12.
-	TH1 = 0x100-((SYSCLK/BAUDRATE)/(2L*12L));
-	TL1 = TH1;      // Init Timer1
-	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit auto-reload
-	TMOD |=  0x20;                       
+	CKCON0 |= 0b_0000_0000; // Timer 1 uses the system clock divided by 12.
+	TH1 = 0x100 - ((SYSCLK / BAUDRATE) / (2L * 12L));
+	TL1 = TH1;	   // Init Timer1
+	TMOD &= ~0xf0; // TMOD: timer 1 in 8-bit auto-reload
+	TMOD |= 0x20;
 	TR1 = 1; // START Timer1
-	TI = 1;  // Indicate TX0 ready
-	
+	TI = 1;	 // Indicate TX0 ready
+
 	return 0;
 }
 
-// Uses Timer3 to delay <us> micro-seconds. 
+// Uses Timer3 to delay <us> micro-seconds.
 void Timer3us(unsigned char us)
 {
-	unsigned char i;               // usec counter
-	
+	unsigned char i; // usec counter
+
 	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON0:
-	CKCON0|=0b_0100_0000;
-	
-	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
-	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
-	
-	TMR3CN0 = 0x04;                 // Sart Timer3 and clear overflow flag
-	for (i = 0; i < us; i++)       // Count <us> overflows
+	CKCON0 |= 0b_0100_0000;
+
+	TMR3RL = (-(SYSCLK) / 1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;					 // Initialize Timer3 for first overflow
+
+	TMR3CN0 = 0x04;			 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++) // Count <us> overflows
 	{
-		while (!(TMR3CN0 & 0x80));  // Wait for overflow
-		TMR3CN0 &= ~(0x80);         // Clear overflow indicator
+		while (!(TMR3CN0 & 0x80))
+			;				// Wait for overflow
+		TMR3CN0 &= ~(0x80); // Clear overflow indicator
 	}
-	TMR3CN0 = 0 ;                   // Stop Timer3 and clear overflow flag
+	TMR3CN0 = 0; // Stop Timer3 and clear overflow flag
 }
 
-void waitms (unsigned int ms)
+void waitms(unsigned int ms)
 {
 	unsigned int j;
-	for(j=ms; j!=0; j--)
+	for (j = ms; j != 0; j--)
 	{
 		Timer3us(249);
 		Timer3us(249);
@@ -126,106 +144,112 @@ void waitms (unsigned int ms)
 
 void TIMER0_Init(void)
 {
-	CKCON0|=0b_0000_0010; // Set Timer0 Clock (T0X2) to 48 clk periods
-	TMOD&=0b_1111_0000; // Set the bits of Timer/Counter 0 to zero
-	TMOD|=0b_0000_0010; // Timer/Counter 0 used as a 8-bit auto-reload timer
-	
-	TL0=RELOAD_TIMER0; // initial value
-	TH0=RELOAD_TIMER0; // reload value
+	CKCON0 |= 0b_0000_0010; // Set Timer0 Clock (T0X2) to 48 clk periods
+	TMOD &= 0b_1111_0000;	// Set the bits of Timer/Counter 0 to zero
+	TMOD |= 0b_0000_0010;	// Timer/Counter 0 used as a 8-bit auto-reload timer
 
-	ET0=1; // enable timer 0 interrupt
-	EA=1; // enable global interrupts
-	
-	TR0=0; // Stop Timer/Counter 0
+	TL0 = RELOAD_TIMER0; // initial value
+	TH0 = RELOAD_TIMER0; // reload value
+
+	ET0 = 1; // enable timer 0 interrupt
+	EA = 1;	 // enable global interrupts
+
+	TR0 = 0; // Stop Timer/Counter 0
 }
-
-
 
 void TIMER2_Init(void)
 {
-	/*
-	CKCON0&=0b_1111_0111; // Set Timer2 Clock (T2X2) to 6 clk periods
-	TMOD&=0b_1111_0000; // Set the bits of Timer/Counter 2 to zero
-	TMOD|=0b_0000_0010; // Timer/Counter 2 used as a 16-bit auto-reload timer
-	
-	TL2=RELOAD_TIMER0; // initial value
-	TH2=RELOAD_TIMER0; // reload value
-
-	ET2=1; // enable timer 0 interrupt
-	EA=1; // enable global interrupts
-	
-	TR0=0; // Stop Timer/Counter 0
-	*/
-	
-
 	// Initialize timer 2 for periodic interrupts
-	TMR2CN0=0x00;   // Stop Timer2; Clear TF2;
-	CKCON0|=0b_0001_0000; // Timer 2 uses the system clock
+	TMR2CN0 = 0x00;			// Stop Timer2; Clear TF2;
+	CKCON0 |= 0b_0001_0000; // Timer 2 uses the system clock
 	// TMR2RL=(0x10000L-(SYSCLK/(2*TIMER_2_FREQ))); // Initialize reload value
-	TMR2RL=29536;
-	TMR2=0xffff;   // Set to reload immediately
-	EA=1; // enable global interrupts
-	ET2=1;         // Enable Timer2 interrupts
-	TR2=0;         // Start Timer2 (TMR2CN is bit addressable)
+	// TMR2RL = (SYSCLK / (24 * 3000)) - 1;
+	TMR2RL = 29536;
+	TMR2 = 0xffff; // Set to reload immediately
+	EA = 1;		   // enable global interrupts
+	ET2 = 1;	   // Enable Timer2 interrupts
+	TR2 = 0;	   // Start Timer2 (TMR2CN is bit addressable)
 }
 
-void Timer0_ISR (void) interrupt INTERRUPT_TIMER0
+void Timer0_ISR(void) interrupt INTERRUPT_TIMER0
 {
-	SFRPAGE=0x0;
+
+	SFRPAGE = 0x0;
+
 	// Timer 0 in 16-bit mode doesn't have auto reload
-	//TMR0=0x10000L-(SYSCLK/(2*TIMER_0_FREQ));
-	TIMER_OUT_0=!TIMER_OUT_0;
+	// TMR0=0x10000L-(SYSCLK/(2*TIMER_0_FREQ));
+	TIMER_OUT_0 = !TIMER_OUT_0;
+	TIMER_OUT_0_INVERTED = !TIMER_OUT_0_INVERTED;
 }
 
-void Timer2_ISR (void) interrupt INTERRUPT_TIMER2
+void Timer2_ISR(void) interrupt INTERRUPT_TIMER2
 {
-	SFRPAGE=0x0;
+	SFRPAGE = 0x0;
 	TF2H = 0; // Clear Timer2 interrupt flag
-	TIMER_OUT_2=!TIMER_OUT_2;
+	// /*
+	// counting
+	pwm_count++;
+	if (pwm_count == 200)
+	{
+		pwm_count = 0;
+		TIMER_OUT_2 = 1;
+		TIMER_OUT_0 = 0;
+		TIMER_OUT_0_INVERTED = 1;
+		TR0 = 1;
+	}
+
+	// pulse width control
+	if (pwm_count == 100)
+	{
+		TIMER_OUT_2 = 0;
+		TR0 = 0;
+	}
+	// */
+	// TIMER_OUT_2=!TIMER_OUT_2;
 }
 
-void LCD_pulse (void)
+void LCD_pulse(void)
 {
-	LCD_E=1;
+	LCD_E = 1;
 	Timer3us(40);
-	LCD_E=0;
+	LCD_E = 0;
 }
 
-void LCD_byte (unsigned char x)
+void LCD_byte(unsigned char x)
 {
 	// The accumulator in the C8051Fxxx is bit addressable!
-	ACC=x; //Send high nible
-	LCD_D7=ACC_7;
-	LCD_D6=ACC_6;
-	LCD_D5=ACC_5;
-	LCD_D4=ACC_4;
+	ACC = x; // Send high nible
+	LCD_D7 = ACC_7;
+	LCD_D6 = ACC_6;
+	LCD_D5 = ACC_5;
+	LCD_D4 = ACC_4;
 	LCD_pulse();
 	Timer3us(40);
-	ACC=x; //Send low nible
-	LCD_D7=ACC_3;
-	LCD_D6=ACC_2;
-	LCD_D5=ACC_1;
-	LCD_D4=ACC_0;
+	ACC = x; // Send low nible
+	LCD_D7 = ACC_3;
+	LCD_D6 = ACC_2;
+	LCD_D5 = ACC_1;
+	LCD_D4 = ACC_0;
 	LCD_pulse();
 }
 
-void WriteData (unsigned char x)
+void WriteData(unsigned char x)
 {
-	LCD_RS=1;
+	LCD_RS = 1;
 	LCD_byte(x);
 	waitms(2);
 }
 
-void WriteCommand (unsigned char x)
+void WriteCommand(unsigned char x)
 {
-	LCD_RS=0;
+	LCD_RS = 0;
 	LCD_byte(x);
 	waitms(5);
 }
 
-void LCD_4BIT (void)
+void LCD_4BIT(void)
 {
-	LCD_E=0; // Resting state of LCD's enable is zero
+	LCD_E = 0; // Resting state of LCD's enable is zero
 	// LCD_RW=0; // We are only writing to the LCD in this program
 	waitms(20);
 	// First make sure the LCD is in 8-bit mode and then change to 4-bit mode
@@ -237,63 +261,66 @@ void LCD_4BIT (void)
 	WriteCommand(0x28);
 	WriteCommand(0x0c);
 	WriteCommand(0x01); // Clear screen command (takes some time)
-	waitms(20); // Wait for clear screen command to finsih.
+	waitms(20);			// Wait for clear screen command to finsih.
 }
 
-void LCDprint(char * string, unsigned char line, bit clear)
+void LCDprint(char *string, unsigned char line, bit clear)
 {
 	int j;
 
-	WriteCommand(line==2?0xc0:0x80);
+	WriteCommand(line == 2 ? 0xc0 : 0x80);
 	waitms(5);
-	for(j=0; string[j]!=0; j++)	WriteData(string[j]);// Write the message
-	if(clear) for(; j<CHARS_PER_LINE; j++) WriteData(' '); // Clear the rest of the line
+	for (j = 0; string[j] != 0; j++)
+		WriteData(string[j]); // Write the message
+	if (clear)
+		for (; j < CHARS_PER_LINE; j++)
+			WriteData(' '); // Clear the rest of the line
 }
 
-int getsn (char * buff, int len)
+int getsn(char *buff, int len)
 {
 	int j;
 	char c;
-	
-	for(j=0; j<(len-1); j++)
+
+	for (j = 0; j < (len - 1); j++)
 	{
-		c=getchar();
-		if ( (c=='\n') || (c=='\r') )
+		c = getchar();
+		if ((c == '\n') || (c == '\r'))
 		{
-			buff[j]=0;
+			buff[j] = 0;
 			return j;
 		}
 		else
 		{
-			buff[j]=c;
+			buff[j] = c;
 		}
 	}
-	buff[j]=0;
+	buff[j] = 0;
 	return len;
 }
 
-void main (void) 
+void main(void)
 {
+	TIMER_OUT_0 = 0;
+	TIMER_OUT_0_INVERTED = 1;
+
+	waitms(500); // Give PuTTY a chance to start.
+	pulse_width = 200;
 	TIMER0_Init();
 	TIMER2_Init();
 
-	waitms(500); // Give PuTTY a chance to start.
 	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
 
-	printf ("EFM8 remote\n"
-	        "File: %s\n"
-	        "Compiled: %s, %s\n\n",
-	        __FILE__, __DATE__, __TIME__);
-	        
+	printf("EFM8 remote\n"
+		   "File: %s\n"
+		   "Compiled: %s, %s\n\n",
+		   __FILE__, __DATE__, __TIME__);
+
 	// Configure the LCD
 	LCD_4BIT();
-	TR0 = 1; // Start Timer0
 	TR2 = 1; // Start Timer2
-	
-    while (1)
-    {
-    	
-    }
-	
-}
 
+	while (1)
+	{
+	}
+}
