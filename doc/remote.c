c23c6100e77d88dc9a48c923c03b452ce9a2bb1a
Nathan Roorda <nathanroorda@gmail.com>
2023-03-28 17:23:10 -0700

    Working Remote Code

--- doc/remote.c
+++ doc/remote.c
@@ -1,8 +1,6 @@
-
 /*
 Timer 0: 5kHz ouput
 Timer 2: 1kHz output -> going to be used for the PWM
-
 */
 
 #include <EFM8LB1.h>
@@ -11,6 +9,7 @@ Timer 2: 1kHz output -> going to be used for the PWM
 
 #define SYSCLK 72000000L // SYSCLK frequency in Hz
 #define BAUDRATE 115200L // Baud rate of UART in bps
+#define SARCLK 18000000L // SARCLK frequency in Hz
 
 // #define RELOAD_TIMER0 106
 #define RELOAD_TIMER0 210 //16kHz
@@ -18,15 +17,10 @@ Timer 2: 1kHz output -> going to be used for the PWM
 #define TIMER_OUT_0 P2_0
 #define TIMER_OUT_0_INVERTED P2_1
 #define TIMER_OUT_2 P1_6
-
-#define ypin1 P0_1
-#define ypin2 P0_2
-#define xpin1 P0_3
-#define xpin2 P0_4
-
-#define beacon P0_5
-
 #define TIMER_2_FREQ = 1000L
+
+#define Bpin P1_3
+#define LEDpin P1_4
 
 #define LCD_RS P2_6
 // #define LCD_RW Px_x // Not used in this code.  Connect to GND
@@ -126,6 +120,32 @@ char _c51_external_startup(void)
 	return 0;
 }
 
+void InitADC (void)
+{
+	SFRPAGE = 0x00;
+	ADEN = 0; // Disable ADC
+	ADC0CN1 = (0x2 << 6) | // 0x0: 10-bit, 0x1: 12-bit, 0x2: 14-bit
+			  (0x0 << 3) | // 0x0: No shift. 0x1: Shift right 1 bit. 0x2: Shift right 2 bits. 0x3: Shift right 3 bits.
+			  (0x0 << 0) ; // Accumulate n conversions: 0x0: 1, 0x1:4, 0x2:8, 0x3:16, 0x4:32
+	ADC0CF0 = ((SYSCLK/SARCLK) << 3) | // SAR Clock Divider. Max is 18MHz. Fsarclk = (Fadcclk) / (ADSC + 1)
+			  (0x0 << 2); // 0:SYSCLK ADCCLK = SYSCLK. 1:HFOSC0 ADCCLK = HFOSC0.
+	ADC0CF1 = (0 << 7) | // 0: Disable low power mode. 1: Enable low power mode.
+			  (0x1E << 0); // Conversion Tracking Time. Tadtk = ADTK / (Fsarclk)
+	ADC0CN0 = (0x0 << 7) | // ADEN. 0: Disable ADC0. 1: Enable ADC0.
+			  (0x0 << 6) | // IPOEN. 0: Keep ADC powered on when ADEN is 1. 1: Power down when ADC is idle.
+			  (0x0 << 5) | // ADINT. Set by hardware upon completion of a data conversion. Must be cleared by firmware.
+			  (0x0 << 4) | // ADBUSY. Writing 1 to this bit initiates an ADC conversion when ADCM = 000. This bit should not be polled to indicate when a conversion is complete. Instead, the ADINT bit should be used when polling for conversion completion.
+			  (0x0 << 3) | // ADWINT. Set by hardware when the contents of ADC0H:ADC0L fall within the window specified by ADC0GTH:ADC0GTL and ADC0LTH:ADC0LTL. Can trigger an interrupt. Must be cleared by firmware.
+			  (0x0 << 2) | // ADGN (Gain Control). 0x0: PGA gain=1. 0x1: PGA gain=0.75. 0x2: PGA gain=0.5. 0x3: PGA gain=0.25.
+			  (0x0 << 0) ; // TEMPE. 0: Disable the Temperature Sensor. 1: Enable the Temperature Sensor.
+	ADC0CF2 = (0x0 << 7) | // GNDSL. 0: reference is the GND pin. 1: reference is the AGND pin.
+			  (0x1 << 5) | // REFSL. 0x0: VREF pin (external or on-chip). 0x1: VDD pin. 0x2: 1.8V. 0x3: internal voltage reference.
+			  (0x1F << 0); // ADPWR. Power Up Delay Time. Tpwrtime = ((4 * (ADPWR + 1)) + 2) / (Fadcclk)
+	ADC0CN2 = (0x0 << 7) | // PACEN. 0x0: The ADC accumulator is over-written. 0x1: The ADC accumulator adds to results.
+			  (0x0 << 0) ; // ADCM. 0x0: ADBUSY, 0x1: TIMER0, 0x2: TIMER2, 0x3: TIMER3, 0x4: CNVSTR, 0x5: CEX5, 0x6: TIMER4, 0x7: TIMER5, 0x8: CLU0, 0x9: CLU1, 0xA: CLU2, 0xB: CLU3
+	ADEN = 1; // Enable ADC
+}
+
 // Uses Timer3 to delay <us> micro-seconds.
 void Timer3us(unsigned char us)
 {
@@ -157,6 +177,46 @@ void waitms(unsigned int ms)
 		Timer3us(249);
 		Timer3us(250);
 	}
+}
+
+#define VDD 3.3035 // The measured value of VDD in volts
+void InitPinADC (unsigned char portno, unsigned char pinno)
+{
+	unsigned char mask;
+	mask = 1 << pinno;
+	SFRPAGE = 0x20;
+	switch (portno)
+	{
+	case 0:
+		P0MDIN &= (~mask); // Set pin as analog input
+		P0SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+	case 1:
+		P1MDIN &= (~mask); // Set pin as analog input
+		P1SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+	case 2:
+		P2MDIN &= (~mask); // Set pin as analog input
+		P2SKIP |= mask; // Skip Crossbar decoding for this pin
+		break;
+	default:
+		break;
+	}
+	SFRPAGE = 0x00;
+}
+
+unsigned int ADC_at_Pin(unsigned char pin)
+{
+	ADC0MX = pin; // Select input from pin
+	ADINT = 0;
+	ADBUSY = 1; // Convert voltage at the pin
+	while (!ADINT); // Wait for conversion to complete
+	return (ADC0);
+}
+
+float Volts_at_Pin(unsigned char pin)
+{
+	return ((ADC_at_Pin(pin)*VDD)/0b_0011_1111_1111_1111);
 }
 
 void TIMER0_Init(void)
@@ -203,7 +263,6 @@ void Timer2_ISR(void) interrupt INTERRUPT_TIMER2
 {
 	SFRPAGE = 0x0;
 	TF2H = 0; // Clear Timer2 interrupt flag
-	// /*
 	// counting
 	pwm_count++;
 	if (pwm_count == 200)
@@ -221,7 +280,6 @@ void Timer2_ISR(void) interrupt INTERRUPT_TIMER2
 		TIMER_OUT_2 = 0;
 		TR0 = 0;
 	}
-	// */
 	// TIMER_OUT_2=!TIMER_OUT_2;
 }
 
@@ -318,8 +376,11 @@ int getsn(char *buff, int len)
 
 void main(void)
 {
-	int x = 0;
-	int y = 0;
+	int x = 1;
+	int y = 1;
+	float NS_Volt;
+	float EW_Volt;
+	int beacon = 1;
 
 	TIMER_OUT_0 = 0;
 	TIMER_OUT_0_INVERTED = 1;
@@ -339,16 +400,60 @@ void main(void)
 	LCD_4BIT();
 	TR2 = 1; // Start Timer2
 
+	InitPinADC(1, 1); // Configure P1.1 as North/South analog input
+	InitPinADC(1, 2); // Configure P1.2 as East/West analog input
+	InitADC();
+
 	while (1)
 	{
-		// TODO: Read the input for x and y
-		
-		// % Duty * 2 = pulse width
-		if(1-beacon){
-			pulse_width = 200; // 100%
+		// Joystick ADC
+		// Read 14-bit value from the pins configured as analog inputs
+		NS_Volt = Volts_at_Pin(QFP32_MUX_P1_1); // North/South Voltage
+		EW_Volt = Volts_at_Pin(QFP32_MUX_P1_2); // East/West Voltage
+
+		// North/South Joystick Coordinates
+		if(NS_Volt < 1.9){
+			y = 2;
+			printf("y = %d\n", y);
+		}
+		else if(NS_Volt > 2.7){
+			y = 0;
+			printf("y = %d\n", y);
 		}
 		else{
-			switch ((2-ypin1-ypin2)*10+(2-xpin1-xpin2)){ // y*10+x
+			y = 1;
+			printf("y = %d\n", y);
+		}
+
+		// East/West Joystick Coordinates
+		if(EW_Volt < 1.9){
+			x = 0;
+			printf("x = %d\n", x);
+		}
+		else if(EW_Volt > 2.7){
+			x = 2;
+			printf("x = %d\n", x);
+		}
+		else{
+			x = 1;
+			printf("x = %d\n", x);
+		}
+		
+		// Joystick Button Toggle
+		if(Bpin){
+			while(Bpin);
+			beacon = !beacon;
+			printf("Beacon Toggled: %d", beacon);
+		}
+		
+		// Variable PWM controlled by joystick
+		if(beacon){
+			pulse_width = 200; // 100%
+			LEDpin = 0; // LED on
+		}
+		else{
+			LEDpin = 1; // LED off
+			switch (y*10+x){ // y*10+x
 				// % Duty * 2 = pulse width
 				case N:
 					pulse_width = 180; // 90%
@@ -384,8 +489,7 @@ void main(void)
 
     			default: // Waiting
 					pulse_width = 20; // 10%
-			}//switch
-
-		}//else
+			}
+		}
 	}
 }
